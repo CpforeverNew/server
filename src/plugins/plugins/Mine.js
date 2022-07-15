82c7e22e3670b05f2d3400e11d1bdc8bbb189e55
Antonio Vallez <arvallez113@students.ecsu.edu>
2022-07-15 17:29:26 -0400

    mine fix

--- src/plugins/plugins/Mine.js
+++ src/plugins/plugins/Mine.js
@@ -6,10 +6,16 @@ export default class Item extends Plugin {
     constructor(users, rooms) {
         super(users, rooms)
         this.events = {
+            'update_coins': this.updateCoins,
             'add_mine_coins': this.addMineCoins,
             'delete_mine' : this.deleteKey
         }
         this.totalcoins = {};
+    }
+
+    async updateCoins(args, user) {
+        let userData = await this.db.getUserById(args.id);
+        user.send('update_coins', {coins: userData.dataValues.coins})
     }
 
     addMineCoins(args, user) {
@@ -17,13 +23,15 @@ export default class Item extends Plugin {
             this.totalcoins[args.miningId] = 0;
         }
 
+        console.log(this.totalcoins)
+
         if (args.coins > 100) {
             user.send('error', {error: 'There was an error adding your coins'});
-            return user.send('mining_error', {miningError:3,total:this.totalcoins})
+            return user.send('mining_error', {miningError:3,total:this.totalcoins,id:args.id})
         } else if (user.lastMined && ((new Date).getTime() - user.lastMined < args.timer - 100)) {
-            return user.send('mining_error', {miningError:2,total:this.totalcoins})
+            return user.send('mining_error', {miningError:2,total:this.totalcoins,id:args.id})
         } else if (this.totalcoins[args.miningId] >= 100) {
-            return user.send('mining_error', {miningError:1,total:this.totalcoins})
+            return user.send('mining_error', {miningError:1,total:this.totalcoins,id:args.id})
         } 
         
         user.updateCoins(args.coins)
@@ -32,12 +40,14 @@ export default class Item extends Plugin {
         if (this.totalcoins[args.miningId] >= 100) {
             return user.send('mining_error', {miningError:1,total:this.totalcoins})
         }
-        return user.send('mining_error', {miningError:0,total:this.totalcoins})
+        return user.send('mining_error', {miningError:0,total:this.totalcoins,id:args.id})
     }
 
     deleteKey(args,user) {
+        if (!(args.miningId in this.totalcoins) || args.miningId === undefined) return;
+
         delete this.totalcoins[args.miningId];
-        return user.send("reset_mining", {})
+        return user.send("reset_mining", {tc:this.totalcoins})
     }
 
 }
