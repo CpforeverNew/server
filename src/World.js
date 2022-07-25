e15c61fc1353008c4edf124a1f5d4c4d626fcd14
Payton Bice <hellopayton@gmail.com>
2022-07-25 17:05:07 -0500

    Merges upstream changes from main

--- src/World.js
+++ src/World.js
@@ -3,23 +3,28 @@ import DataHandler from './handlers/DataHandler'
 import LoginHandler from './handlers/LoginHandler'
 import Server from './server/Server'
 import Discord from './logging/Discord'
-
 import config from '../config/config.json'
+import { Client, Intents } from 'discord.js'
 
+const discordApiClient = new Client({
+  intents: [Intents.FLAGS.GUILDS],
+})
+
+discordApiClient.once('ready', () => console.info('Discord client connected successfully'))
+
+discordApiClient.login(process.env.DISCORD_TOKEN)
 
 class World extends Server {
+  constructor(id) {
+    let users = {}
+    let db = new Database(config.database)
+    let discord = new Discord(discordApiClient)
 
-    constructor(id) {
-        let users = {}
-        let db = new Database(config.database)
-        let discord = new Discord(config)
+    let handler = (id == 'Login') ? LoginHandler : DataHandler
+    handler = new handler(id, users, db, config, discord)
 
-        let handler = (id == 'Login') ? LoginHandler : DataHandler
-        handler = new handler(id, users, db, config, discord)
-
-        super(id, users, db, handler, config)
-    }
-
+    super(id, users, db, handler, config)
+  }
 }
 
 let args = process.argv.slice(2)
--- src/plugins/plugins/Chat.js
+++ src/plugins/plugins/Chat.js
@@ -1,9 +1,8 @@
 import Plugin from '../Plugin'
+import { getFilterResultByText } from '../../services/filter/filter'
+import { PerspectiveAPIClientError } from 'perspective-api-client'
 
-import Perspective from 'perspective-api-client'
-
-export default class    Chat extends Plugin {
-
+export default class Chat extends Plugin {
     constructor(users, rooms) {
         super(users, rooms)
         this.events = {
@@ -20,15 +19,11 @@ export default class    Chat extends Plugin {
         }
 
         this.bindCommands()
-
-        this.perspective = new Perspective({
-            apiKey: this.config.perspectiveapikey
-        });
     }
 
     // Events
 
-    sendMessage(args, user) {
+    async sendMessage(args, user) {
         // Todo: message validation
         if (args.message.startsWith('!')) {
             return this.processCommand(args.message.substring(1), user)
@@ -37,85 +32,46 @@ export default class    Chat extends Plugin {
         if (args.message.length < 1) {
             return
         }
-        (async () => {
-            try {
-                const result = await this.perspective.analyze({
-                    "comment": {
-                        text: args.message
-                    },
-                    "languages": ["en"],
-                    "requestedAttributes": {
-                        "TOXICITY": {},
-                        "SEXUALLY_EXPLICIT": {},
-                        "PROFANITY": {}
-                    }
-                });
-                const toxicity = result.attributeScores.TOXICITY.summaryScore.value * 100
-                const profanity = result.attributeScores.PROFANITY.summaryScore.value * 100
-                const sexual = result.attributeScores.SEXUALLY_EXPLICIT.summaryScore.value * 100
-                if (toxicity > 60 || profanity > 60 || sexual > 60) this.discord.logChatMessage(user.data.username, args.message, user.room.name, toxicity.toString().split(".")[0], profanity.toString().split(".")[0], sexual.toString().split(".")[0])
-                if (profanity > 91 || sexual > 91) {
-                    user.send('error', {
-                        error: "Your message was not sent because our chat filter deems it unsuitable for the safe environment of CPF."
-                    })
-                    user.room.send(user, 'filtered_message', {
-                        id: user.data.id,
-                        message: args.message,
-                        filter: 'perspectiveapi'
-                    }, [], true)
-                    user.logChat(args.message, true, 'perspectiveapi')
-                    return
-                }
-            } catch (err) {
-                this.discord.errorAlert("Perspective API Error")
+
+        try {
+            const result = await getFilterResultByText(args.message)
+
+            if (result.shouldTextBeLogged()) {
+                this.discord.logChatMessage(
+                    user.data.username,
+                    args.message,
+                    user.room.name,
+                    result
+                )
             }
 
-            var specificFilterWords = ['ass', 'hoe', 'nga', "fu", "af", "asf", "hell", "dic", "fk"]
-            var filterWords = ["nigger","nligga","nigga","faggot","niggr","ngga","fck","niqqa","niqqer","faqqot","faggt","fag","faq","fuq","fuk","nood","nudes","noodes","slxt","cxnt","b!tch","bish","bich","bxtch","pxssy","poosy","pusea","pusy","bunda","puzzy","arse","azz","nxgger","niger","n!ger","n!g","nigg","nxgga","nga","nger","fxck","fucc","fuc","fuk","fxk","phuck","phuc","phxc","cxm","vxg","vag","forn","porn","pen!s","pxn!s","pxnis","penxs","mxther","motherfxcker","mofo","whore","whxre","whor","coc","cxck","sh!t","effin","eff!n","effxn","twt","twxt","thwat","sxx","sex","tass","ashole","ashxle","d!c","d!k","dik","bxxb","boob","lmao","lmfao","cuck","peen","penls","dick","hoes","stfu","cunt","handjob","blowie","drugs","damn","kkk","piss","penis","tiddies","bitch","slut","shit","kill","suicide","lawda","tiddy","titties","titty","pussy","ussy","weiner","wiener","omfg", "ween","undress","threesome","orgy","intercourse","nipples","vibrator","anus","anal","ejaculate","vulva","clit","cum","rape","fuddi","genital","chlamydia","aids","hiv","segsy", "segg", "nude","biddies","squirting","sect","tf","orgasm","creampie","creamed","oral","dam","vape","tequila","vodka","weed","bourbon","nug","roach","marijuana","alcohol","queef","blowjob","blow","ovulating","punani","puss","sperm","fertile","twat"];
-            var wordWhitelist = ['shoes', 'afk', 'coco', 'engage', 'echoes', 'snowshoes', 'horseshoes', 'shoestrings', 'offkey', 'saltwater', 'wristwatch', 'outfit', 'longer', 'thoughtful', 'halloween', 'mermaid', 'restful', 'therapeutic', 'ginger', 'singer', 'grape', 'noodle', 'vague', 'floral', 'skills', 'rubbish', 'california', 'nugget', 'between', 'jazz', 'circumstance', 'approach', 'anger', 'analysis', 'analyze'];
-            wordWhitelist += ['conga','linger', 'stinger', 'hanger', 'challenger', 'finger', 'doppelganger', 'passenger', 'scavenger', 'endanger', 'endangered', 'danger', 'dangers', 'dangerous', 'messenger', 'outfits', 'outfit', 'stranger']
-
-            var words = args.message.toLowerCase().split(" ")
-
-            var contains = false
-            var offendingWord = ""
-
-            for (var x in words) {
-                if (filterWords.includes(words[x]) || specificFilterWords.includes(words[x])) {
-                    contains = true
-                    offendingWord = words[x]
-                    continue
-                }
-                if (!wordWhitelist.includes(words[x])) {
-                    filterWords.some(element => {
-                        if (words[x].includes(element)) {
-                            contains = true
-                            offendingWord = element
-                        }
-                    })
-                }
-            }
-
-            if (!contains) {
-                user.messagesSentThisSession++
-                user.room.send(user, 'send_message', {
-                    id: user.data.id,
-                    message: args.message
-                }, [], true)
-                user.logChat(args.message)
-            }
-            else {
+            if (result.shouldBeFiltered()) {
                 user.send('error', {
-                    error: "Your message was not sent because our chat filter deems it unsuitable for the safe environment of CPF. Please let us know if you think this was an error!"
+                    error: "Your message was not sent because our chat filter deems it unsuitable for the safe environment of CPF."
                 })
+
                 user.room.send(user, 'filtered_message', {
                     id: user.data.id,
                     message: args.message,
-                    filter: 'manual'
+                    filter: result.filter,
                 }, [], true)
-                user.logChat(args.message, true, `manual filter because of ${offendingWord}`)
+
+                return user.logChat(args.message, true, result.filter)
             }
-        })();
+
+            user.messagesSentThisSession++
+
+            user.room.send(user, 'send_message', {
+                id: user.data.id,
+                message: args.message,
+            }, [], true)
+
+            user.logChat(args.message)
+        } catch (e) {
+            if (e instanceof PerspectiveAPIClientError) {
+                this.discord.errorAlert("Perspective API Error")
+            }
+        }
     }
 
     sendSafe(args, user) {
